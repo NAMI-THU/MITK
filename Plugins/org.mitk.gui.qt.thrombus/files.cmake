5a84e402d24b3d31d32267fcfd9b1c00251dab4d
Tobias Kannberg <kannbergtobias@gmail.com>
2021-05-24 15:56:33 +0200

    Removed constructor & destructor

--- Plugins/org.mitk.gui.qt.thrombus/files.cmake
+++ Plugins/org.mitk.gui.qt.thrombus/files.cmake
@@ -5,6 +5,7 @@ set(SRC_CPP_FILES
 set(INTERNAL_CPP_FILES
   org_mitk_gui_qt_thrombus_Activator.cpp
   QmitkThrombusView.cpp
+  QmitkThrombusRegistration.cpp
 )
 
 set(UI_FILES
@@ -14,6 +15,7 @@ set(UI_FILES
 set(MOC_H_FILES
   src/internal/org_mitk_gui_qt_thrombus_Activator.h
   src/internal/QmitkThrombusView.h
+  src/internal/QmitkThrombusRegistration.h
 )
 
 # list of resource files which can be used by the plug-in
