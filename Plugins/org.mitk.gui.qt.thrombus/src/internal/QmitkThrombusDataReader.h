e637d94d5cbc1fc0a562c23205cc5aec2a24ea25
Tobias Kannberg <kannbergtobias@gmail.com>
2021-05-26 15:21:45 +0200

    Added includes

--- Plugins/org.mitk.gui.qt.thrombus/src/internal/QmitkThrombusDataReader.h
+++ Plugins/org.mitk.gui.qt.thrombus/src/internal/QmitkThrombusDataReader.h
@@ -4,12 +4,22 @@
 // blueberry headers
 #include <berryISelectionListener.h>
 
+// mitk headers
+#include <MitkIGTExports.h>
+#include <mitkNavigationToolStorage.h>
+#include <mitkTrackingDevice.h>
+#include <mitkTrackingDeviceSource.h>
+#include <mitkCommon.h>
 
 // Qt headers
 #include <QTimer>
 
 class QmitkThrombusDataReader
 {
+  mitk::TrackingDeviceSource::Pointer trackingSource;
+
+
+  void SetTrackingDevice(mitk::TrackingDeviceSource::Pointer trackingSource);
 };
 
 #endif // QMITKTHROMBUSDATAREADER_H
