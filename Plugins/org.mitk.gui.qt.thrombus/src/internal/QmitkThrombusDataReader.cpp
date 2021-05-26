6c58e6caceb97cb492b9cb080545dd192511be41
Tobias Kannberg <kannbergtobias@gmail.com>
2021-05-26 12:29:13 +0200

    Class QmitkThrombusDataReader introduced

---
+++ Plugins/org.mitk.gui.qt.thrombus/src/internal/QmitkThrombusDataReader.cpp
@@ -0,0 +1,1 @@
+#include "QmitkThrombusDataReader.h"
---
+++ Plugins/org.mitk.gui.qt.thrombus/src/internal/QmitkThrombusDataReader.h
@@ -0,0 +1,15 @@
+#ifndef QMITKTHROMBUSDATAREADER_H
+#define QMITKTHROMBUSDATAREADER_H
+
+// blueberry headers
+#include <berryISelectionListener.h>
+
+
+// Qt headers
+#include <QTimer>
+
+class QmitkThrombusDataReader
+{
+};
+
+#endif // QMITKTHROMBUSDATAREADER_H
