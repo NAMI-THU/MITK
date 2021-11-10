0b23ad80c517092c09329e5b1a486f200f5717b3
Tobias Kannberg <kannbergtobias@gmail.com>
2021-11-10 12:05:37 +0100

    Started including ARStrokeTreatmentRegistration

--- Plugins/org.mitk.gui.qt.ARStrokeTreatment/src/internal/ARStrokeTreatmentRegistration.cpp
+++ Plugins/org.mitk.gui.qt.ARStrokeTreatment/src/internal/ARStrokeTreatmentRegistration.cpp
@@ -16,3 +16,6 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include "ARStrokeTreatmentRegistration.h"
 
+ARStrokeTreatmentRegistration::InitializeRegistration() {
+
+}
--- Plugins/org.mitk.gui.qt.ARStrokeTreatment/src/internal/ARStrokeTreatmentRegistration.h
+++ Plugins/org.mitk.gui.qt.ARStrokeTreatment/src/internal/ARStrokeTreatmentRegistration.h
@@ -17,7 +17,35 @@ See LICENSE.txt or http://www.mitk.org for details.
 #ifndef ARStrokeTreatmentRegistration_h
 #define ARStrokeTreatmentRegistration_h
 
+#include <QmitkAbstractView.h>
 
+/**
+  \brief ARStrokeTreatmentView
 
+  \warning  This class is not yet documented. Use "git blame" and ask the author to provide basic documentation.
+
+  \sa QmitkAbstractView
+  \ingroup ${plugin_target}_internal
+*/
+
+class ARStrokeTreatmentRegistration : public QmitkAbstractView
+{
+  // this is needed for all Qt objects that should have a Qt meta-object
+  // (everything that derives from QObject and wants to have signal/slots)
+  Q_OBJECT
+
+public:
+  void SetFocus() override;
+
+  static const std::string VIEW_ID;
+
+  void CreateQtPartControl(QWidget *parent) override;
+
+  ARStrokeTreatmentRegistration();
+  ~ARStrokeTreatmentRegistration() override;
+
+protected:
+  void InitializeRegistration();
+};
 
 #endif // ARStrokeTreatmentRegistration_h
