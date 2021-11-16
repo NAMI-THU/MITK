004a822700fffc02db87f1086a8fae9deeb29737
Tobias Kannberg <kannbergtobias@gmail.com>
2021-11-16 09:32:19 +0100

    Deleted Thrombus plugin

--- Plugins/PluginList.cmake
+++ Plugins/PluginList.cmake
@@ -56,7 +56,6 @@ set(MITK_PLUGINS
   org.mitk.gui.qt.aicpregistration:OFF
   org.mitk.gui.qt.renderwindowmanager:OFF
   org.mitk.gui.qt.semanticrelations:OFF
-  org.mitk.gui.qt.thrombus:ON
   org.mitk.gui.qt.toftutorial:OFF
   org.mitk.gui.qt.tofutil:OFF
   org.mitk.gui.qt.tubegraph:OFF
